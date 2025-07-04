9b7704584d67e65fe127c745b4f3fdd08c01d9ba
Okeze Kingsley <okezekingsley18@gmail.com>
2025-07-04 19:40:48 +0100

    account setup fix

--- controllers/user/updateUser.js
+++ controllers/user/updateUser.js
@@ -5,17 +5,17 @@ const {_id} = req.user
 
 try {
 if(!type) {
-    return res.status(400).json({message: "Type is required"})
+    return res.status(400).json({error: "Type is required"})
 }
 
 if(!["tenant", "landlord"].includes(type)) {
-      return res.status(400).json({message: "Type must be either tenant or landlord"})
+      return res.status(400).json({error: "Type must be either tenant or landlord"})
 }
 
 const updateduser = await AuthCollection.findByIdAndUpdate(_id, {$set : {accountType : type}}, {new : true})
 return res.status(201).json(updateduser)
 } catch (error) {
-return res.status(500).json({message: "Account update failed"})
+return res.status(500).json({error: "Account update failed"})
 }
 
 }
